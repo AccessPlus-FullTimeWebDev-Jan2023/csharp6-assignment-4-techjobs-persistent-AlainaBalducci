344b60df01f9ea0ac302d78777545a3b3cf88d7a
Alaina Balducci <alainabalducci@gmail.com>
2023-04-06 15:44:07 -0500

    Possibly finished the about IActionResult Method

--- TechJobs6Persistent/Controllers/EmployerController.cs
+++ TechJobs6Persistent/Controllers/EmployerController.cs
@@ -45,13 +45,29 @@ namespace TechJobs6Persistent.Controllers
         }
 
         [HttpPost]
-        public IActionResult ProcessCreateEmployerForm()
+        public IActionResult ProcessCreateEmployerForm(AddEmployerViewModel addEmployerViewModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                Employer newEmployer = new Employer
+                {
+                    Name = addEmployerViewModel.Name,
+                    Location = addEmployerViewModel.Location
+                };
+
+                context.Employers.Add(newEmployer);
+                context.SaveChanges();
+
+                return Redirect("Employer");
+            }
+            return View("Create", addEmployerViewModel);
         }
 
         public IActionResult About(int id)
         {
+            
+            List<Employer> employers = new List<Employer>();
+            employers.Find(Employer =>  Employer.Id == id);
             return View();
         }
 
